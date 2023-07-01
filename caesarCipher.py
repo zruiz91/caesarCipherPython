8efadd0558b4225a189a5adb52154b1e874ecc69
Zach Ruiz <powersbooth10@gmail.com>
2023-07-01 14:35:04 -0500

    refactorde code.

--- caesarCipher.py
+++ caesarCipher.py
@@ -7,11 +7,11 @@ shift = int(input("Type the shift number:\n"))
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 
 
-def encrypt(text, shift):
+def encrypt(plain_text, shift_amount):
   cipher_text_list = []
-  for letter in text:
+  for letter in plain_text:
     print(alphabet[-1])
-    new_index = alphabet.index(letter) + shift
+    new_index = alphabet.index(letter) + shift_amount
     if new_index > 25:
       new_index = new_index - 26
     print(new_index)
@@ -34,6 +34,6 @@ def encrypt(text, shift):
 
 #TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
 if direction == "encode":
-  encrypt(text, shift)
+  encrypt(plai_text, shift_amount)
 elif direction == "decode":
   decrypt(text, shift)
