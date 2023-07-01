d639837766db89b3fb30085f0e7461357ba7f651
Zach Ruiz <powersbooth10@gmail.com>
2023-07-01 14:49:46 -0500

    added working code for the decrypt function.

--- caesarCipher.py
+++ caesarCipher.py
@@ -1,4 +1,4 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
@@ -10,30 +10,25 @@ shift = int(input("Type the shift number:\n"))
 def encrypt(plain_text, shift_amount):
   cipher_text_list = []
   for letter in plain_text:
-    print(alphabet[-1])
     new_index = alphabet.index(letter) + shift_amount
-    if new_index > 25:
-      new_index = new_index - 26
     print(new_index)
     cipher_text_list.append(alphabet[new_index])
 
   cipher_text_string = ''.join(cipher_text_list)
   print(f"The encoded text is {cipher_text_string}")
   
-    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
-    #e.g. 
-    #plain_text = "hello"
-    #shift = 5
-    #cipher_text = "mjqqt"
-    #print output: "The encoded text is mjqqt"
+def decrypt(cipher_text, shift_amount):
+  plain_text_list = []
+  for letter in cipher_text:
+    new_index = alphabet.index(letter) - shift_amount
+    print(new_index)
+    plain_text_list.append(alphabet[new_index])
 
-    ##HINT: How do you get the index of an item in a list:
-    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
+  plain_text_string = ''.join(plain_text_list)
+  print(f"The encoded text is {plain_text_string}")
 
-    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
 
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
 if direction == "encode":
-  encrypt(plai_text = text, shift_amount = shift)
+  encrypt(plain_text = text, shift_amount = shift)
 elif direction == "decode":
-  decrypt(text, shift)
+  decrypt(cipher_text = text, shift_amount = shift)
